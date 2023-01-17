5876e4a71d9bf36dd913f9218895bec2f85a7714
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-17 10:38:12 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-
-  resources :post_images, only: [:new, :create, :index, :show, :destroy ]
-
   get '/homes/about', as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy ]
+  resources :users, only: [:show, :edit]
 end
 
 
