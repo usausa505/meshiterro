a26333c0537de9833dc4482552a90e875026b2a2
Shingo Saeki <110162627+usausa505@users.noreply.github.com>
2023-01-18 05:53:30 +0000

    [Finish] 4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,15 +14,16 @@ class PostImagesController < ApplicationController
   def index
     @post_images = PostImage.all
   end
- 
+
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
   def destroy
-    @post_image = 削除するPostImageレコードを取得
-    @post_image.削除
-    redirect_to PostImageの一覧ページのパス
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to '/post_images'
   end
 
   private
@@ -30,5 +31,5 @@ class PostImagesController < ApplicationController
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
